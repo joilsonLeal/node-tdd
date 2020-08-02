6dc95818ad64b042f49ca9eadb2645b67e44405c
Joilson <joilson_rg@hotmail.com>
2020-08-02 00:56:55 +0000

    feature: criação de teste para verificar se senha está encriptografada

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,11 +1,25 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
+import User from '../../src/app/models/User';
 import truncate from '../util/truncate';
 
 describe('User', () => {
   beforeEach(async() => {
     await truncate();
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Diego Fernandes',
+      email: 'fernandes@gmail.com',
+      password: '8712381623',
+    });
+
+    const compareHash = await bcrypt.compare('8712381623', user.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 
   it('should be able to register', async () => {
