a0a12cd6d87ba1b0a8b24bf31b377ed0e323ef84
Joilson <joilson_rg@hotmail.com>
2020-08-02 00:49:55 +0000

    feature: realiza truncate antes de cada teste

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async() => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -14,7 +20,7 @@ describe('User', () => {
     expect(response.body).toHaveProperty('id');
   });
 
-  if(`shouldn't be able to register with duplicated email`, async () => {
+  it(`shouldn't be able to register with duplicated email`, async () => {
     await request(app)
       .post('/users')
       .send({
