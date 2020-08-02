36f3e590b0c323926b5ae245e4139fc2b8b43274
Joilson <joilson_rg@hotmail.com>
2020-08-02 00:38:39 +0000

    chore: remoção do arquivo com o postgres

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -12,6 +12,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
 
+  if(`shouldn't be able to register with duplicated email`, async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'fernandes@gmail.com',
+        password_hash: '8712381623',
+    });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'fernandes@gmail.com',
+        password_hash: '8712381623',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
